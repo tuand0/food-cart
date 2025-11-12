51fc2d7cc106077ce160b23163a218123dec547f
tuando <tuando@NORBERT>
2025-11-12 21:01:36 +0100

    upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shopping-cart';
+
+  protected readonly RouterOutlet = RouterOutlet;
 }
